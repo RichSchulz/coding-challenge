bfbee4542a8f511b37646c1324497f1ee85607e7
Richard Schulz <richard.schulz@student.hpi.de>
2021-11-02 19:22:23 +0100

    Improved documentation and added test for empty list

--- solution/src/fold.py
+++ solution/src/fold.py
@@ -10,7 +10,14 @@ def fold(sequence: List[A],
          starting_value: B,
          function: Callable[[A, B], B],
          right: bool = True) -> B:
-    """Custom implementation of the higher order function fold"""
+    """Custom implementation of the higher order function fold
+    Parameters:
+        sequence (List): The sequence to be folded
+        starting_value (T): The starting value of the folding
+        function (Callable): The function to be used for folding
+        right (bool): If true a right fold is performed, else a left fold
+    Returns:
+        T: The result of the folding"""
     # If a left fold should be performed
     # the computation order has to be changed by reversing the sequence.
     if not right:
--- solution/test/test_fold.py
+++ solution/test/test_fold.py
@@ -3,8 +3,8 @@ import pytest
 from solution.src.fold import fold
 
 
-ADD = (lambda a, b: a + b)
-MULTIPLY = (lambda a, b: a * b)
+ADD = lambda a, b: a + b
+MULTIPLY = lambda a, b: a * b
 
 
 @pytest.mark.parametrize(
@@ -16,20 +16,24 @@ MULTIPLY = (lambda a, b: a * b)
         ([1, 2, 3], 0, ADD, True, 6)
     ]
 )
-def test_fold_valid_input(sequence, starting_value, function, right, expected):
+def test_valid_input(sequence, starting_value, function, right, expected):
     assert fold(sequence, starting_value, function, right) == expected
 
 
-def test_fold_no_list():
-    with pytest.raises(Exception):
+def test_empty_list():
+    assert fold([], 1, ADD) == 1
+
+
+def test_no_list():
+    with pytest.raises(TypeError):
         fold(1, 1, ADD)
 
 
-def test_fold_no_function():
-    with pytest.raises(Exception):
+def test_no_function():
+    with pytest.raises(TypeError):
         fold([1], 1, 1)
 
 
-def test_fold_different_types():
-    with pytest.raises(Exception):
+def test_different_types():
+    with pytest.raises(TypeError):
         fold([1, 2], "3", 6)
