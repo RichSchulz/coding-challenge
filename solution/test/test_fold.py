5860b72295c344a00da4f3f26ccd63d4d5b4111c
Richard Schulz <richard.schulz@student.hpi.de>
2021-11-02 13:36:08 +0100

    Created first (simple) solution

---
+++ solution/test/test_fold.py
@@ -0,0 +1,18 @@
+import pytest
+
+from solution.src.fold import fold
+
+
+ADD = (lambda a,b: a + b)
+MULTIPLY = (lambda a,b: a * b)
+
+@pytest.mark.parametrize(
+    "a,b,c,expected",
+    [
+        ([1, 2, 3], 0, ADD, 6),
+        ([0, 0 , 0, 0], 5, ADD, 5),
+        ([2, 2 , 2, 2], 1, MULTIPLY, 16),
+    ]
+)
+def test_fold(a, b,c , expected):
+    assert fold(a, b, c) == expected
