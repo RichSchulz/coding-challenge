7ff47327674ca595ae812344316d897f496ef693
Richard Schulz <richard.schulz@student.hpi.de>
2021-11-02 13:55:58 +0100

    Linted first solution to check CI

--- solution/src/fold.py
+++ solution/src/fold.py
@@ -1,8 +1,10 @@
 from typing import Callable, List
 
 
-# TODO: Check if types match, maybe add class for function, maybe add custom exception
-def fold(sequence: List[any], starting_value: any, function: Callable[[any, any], any]):
+# TODO: Check if types match, maybe add class for function & custom exception
+def fold(sequence: List[any],
+         starting_value: any,
+         function: Callable[[any, any], any]):
     for value in sequence:
         starting_value = function(starting_value, value)
     return starting_value
